08d8dce50c45ea659ec273d6ba22e6dd62b40055
Marina Portugal <marinapsvreis@gmail.com>
2023-04-17 18:39:05 -0300

    :lipstick: Configurando estilos globais

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -24,9 +24,13 @@ interface HomeProps {
   postsPagination: PostPagination;
 }
 
-// export default function Home() {
-//   // TODO
-// }
+export default function Home() {
+  return (
+    <>
+      <h1>Home</h1>
+    </>
+  );
+}
 
 // export const getStaticProps = async () => {
 //   // const prismic = getPrismicClient({});
